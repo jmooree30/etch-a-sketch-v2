8c4689284c76ea4e149d67132133caed9344352a
Jacob <jmooree30@gmail.com>
2018-01-11 00:59:31 -0500

    need to fix random color

--- etch.js
+++ etch.js
@@ -1,20 +1,24 @@
-let n=10;
+let n=50;
 let boxSize = 500/n
 let boxWidth = 500/n*n;
+var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
 document.querySelector(".container").style.width = boxWidth;
+document.querySelector(".container").style.height = "500px";
 
 function grid(){
-  for(var i = 1; i < n; i++) {
-    document.getElementById('container').innerHTML+='<div class="row">'
+  for(var i = 0; i < n; i++) {
+    var div = document.createElement('div');
+    document.getElementById('container').appendChild(div);
 
     for(k = 0; k < n; k++) {
-     document.getElementById('container').innerHTML+='<span id="gridsquare" class="gridsquare"></span>'; 
+     var span = document.createElement('span');
+     span.setAttribute('class', 'gridsquare');
+     document.getElementById('container').appendChild(span); 
    } 
  }
-  buttonListen()
-  squareSize()
-  hover()
+ squareSize()
+ hover("black")
 }
 
 function squareSize(){
@@ -26,26 +30,45 @@ function squareSize(){
   });
 }
 
-function hover(){ 
+function hover(purple){ 
   let hover = document.querySelectorAll(".gridsquare");
   for (i=0;i<hover.length;i++){
     hover[i].addEventListener("mouseenter", function(each) { 
-      each.target.style.background = "purple";
+      each.target.style.background = purple;
     });
   }
 }
 
 function clearGrid(){
   let hover = document.querySelectorAll(".gridsquare");
-  let density = window.prompt("Enter pixel density 1-100")
-  let n = density
   for (i=0;i<hover.length;i++){
-    hover[i].style.background = "white";
+    hover[i].remove()};
+    n = window.prompt("Enter grid density. 1-100")
+    boxSize = 500/n
+    boxWidth = 500/n*n;
+    grid()
   }
-}
 
-function buttonListen(){
-  let button = document.querySelector("button");
-  button.addEventListener("click", clearGrid);
+  function buttonListen(){
+    let button = document.querySelector("button");
+    button.addEventListener("click", clearGrid);
+  }
+
+
+    let orange = document.getElementById('one');
+    orange.addEventListener('click', function(){ hover("orange")},false);
+    
+    let red = document.getElementById('two');
+    red.addEventListener('click', function(){ hover("red")},false);
+
+    let rand = document.getElementById('three');
+    rand.addEventListener('click', function(){ hover(hue)},false);
+
+
+function randColor(){
+    let all = document.querySelectorAll(".gridsquare");
+    for(i=0;i<all.length;i++){
 }
-grid()
+}
+  grid()
+  buttonListen()
